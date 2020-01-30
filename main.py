bd64e10162979e88290112062a5a005b4eea76cb
Javier Arias <apedro@unicauca.edu.co>
2020-01-29 21:26:41 -0500

    Menu libros terminado

--- main.py
+++ main.py
@@ -44,13 +44,13 @@ def menuLibros():
         if entrada == "1":
             leer()
         elif entrada == "2":
-            pass
+            addTextoLibro()
         elif entrada == "3":
             listarLibrosPorAutor()
         elif entrada == "4":
             add_libros()
         elif entrada == "5":
-            pass
+            del_libros()
         elif entrada == "6":
             menuPrincipal()
         elif entrada == "7":
@@ -92,10 +92,12 @@ def agregar_texto():
     return salida
 
 def addMetadatos(titulo, autor):
+    pais = input("Digite Pais: ")
+    anio = input("Digite año: ")
     texto =" Autor: "+autor+"\n"
     texto = texto + " Titulo libro: " + titulo+"\n"
-    texto = texto + " País: Desconocido \n"
-    texto = texto + " Año: Desconocido \n"
+    texto = texto + " País: "+pais+" \n"
+    texto = texto + " Año: "+anio+" \n"
     texto = texto + "_________________________________________________________________________________________\n"
     return texto
 
@@ -108,7 +110,7 @@ def add_libros():
              print("El libro ya existe!")
         else:
             libro_nuevo = open(carpeta_autor + titulo + ".txt", "a+")
-            libro_nuevo.write(agregar_texto())
+            libro_nuevo.write(addMetadatos(titulo, autor) + agregar_texto())
             libro_nuevo.close()
     else:
         os.mkdir(RUTA+autor)
@@ -116,14 +118,39 @@ def add_libros():
         libro_nuevo.write(addMetadatos(titulo, autor)+agregar_texto())
         libro_nuevo.close()
 
+def del_libros():
+    titulo = input("Ingrese el titulo: ")
+    autor = input("Ingrese el autor: ")
+    carpeta_autor = RUTA + autor+"/"
+    if(os.path.isdir(carpeta_autor)):# la funcio os.path.isdir confirma si es directorio o no
+        if(os.path.isfile(carpeta_autor+titulo+".txt")):#---la funcion os.path.isfile confirma si es un archivo o no
+            os.remove(carpeta_autor+titulo+".txt")
+        else:
+            print("El libro NO existe")
 
+    else:
+        print("El autor NO existe")
+
+def addTextoLibro():
+    titulo = input("Ingrese el titulo: ")
+    autor = input("Ingrese el autor: ")
+    carpeta_autor = RUTA + autor+"/"
+    if(os.path.isdir(carpeta_autor)):# la funcio os.path.isdir confirma si es directorio o no
+        if(os.path.isfile(carpeta_autor+titulo+".txt")):#---la funcion os.path.isfile confirma si es un archivo o no
+            libro_nuevo = open(carpeta_autor + titulo + ".txt", "a+")
+            libro_nuevo.write(agregar_texto())
+            libro_nuevo.close()
+        else:
+            print("El libro NO existe!")
+    else:
+        print("El Autor NO existe!")
 
 def listarLibrosPorAutor():
     Autores = list(filter(os.path.isdir, os.listdir(RUTA)))
     Autores.sort()
     for i in Autores:
         if(os.path.isdir(RUTA+i+"/")):# Confirma si está la carpeta
-            print("ruta: "+RUTA+i+"/"+"Autor: "+i)#Imprime Autor
+            print("Autor: "+i)#Imprime Autor
             for j in os.listdir(RUTA+i):
                 print("    "+j)#Imprime sus libros
 
@@ -132,9 +159,9 @@ def leer():
     autor = input("Ingrese el autor: ")
     if (os.path.isdir(RUTA + autor+"/")):
         if(os.path.isfile(RUTA + autor+"/"+titulo+".txt")):#---la funcion os.path.isfile confirma si es un archivo o no
-            libro_nuevo = open(RUTA+autor+"/"+titulo+".txt", "r")
-            print("Es es la direccion: "+RUTA+autor+"/"+titulo+".txt")
-            libro_nuevo.read()
+            libro_nuevo = open(RUTA+autor+"/"+titulo+".txt", "r+")
+            texto = libro_nuevo.read()
+            print(texto)
             libro_nuevo.close()
 
 #--------incio main
